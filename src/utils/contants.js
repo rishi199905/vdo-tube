0261bc749b01163db83c70f1addbb74a399c1e15
rishikesh bhattalwar <bhattalwarr@gmail.com>
2025-07-03 12:42:19 +0530

    api change

--- src/utils/contants.js
+++ src/utils/contants.js
@@ -3,7 +3,7 @@
 export const LIVE_CHAT_COUNT = 25;
 
 export const YOUTUBE_SEARCH_API =
-  "http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
+  "https://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
 
 export const YOUTUBE_VIDEOS_API = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=US&key=AIzaSyBinhBil5aL9Z6HsMvrXATo6VMoLw1Zabo"
 // const API_KEY = "AIzaSyBinhBil5aL9Z6HsMvrXATo6VMoLw1Zabo"
